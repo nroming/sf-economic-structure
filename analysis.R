0988f92d8c4880f5efd126586b617ffb41576844
Niklas Roming <niklas.roming@climateanalytics.org>
2016-09-30 11:02:37 +0200

    move plotting to separate file

--- analysis.R
+++ analysis.R
@@ -5,7 +5,7 @@ library(ggplot2)
 rm(list = ls())
 
 # create output directories
-dir.create("plots", showWarnings = FALSE)
+if(!dir.exists("plots")) dir.create("plots", showWarnings = FALSE)
 
 # prepare data ----
 # vector of variables
@@ -53,55 +53,6 @@ wdi <- group_by(wdi, spatial) %>%
                  gdp_pc_gr = lag(gdp_pc, n = 0, order_by = temporal) / lag(gdp_pc, n = 1, order_by = temporal) - 1) %>%
           ungroup()
 
-# plot: sectoral value added per capita over gdp per capita ----
-df_plot <- filter(wdi, spatial %in% g20) %>% select(spatial, temporal,
-                                                    va_agr_pc, va_ind_pc,
-                                                    va_ser_pc, gdp_pc)
-
-df_plot <- df_plot[complete.cases(df_plot), ]
-
-df_plot <- melt(df_plot, id.vars = c("spatial", "temporal", "gdp_pc"))
-
-# order countries according to their 2013 GDP levels
-df_plot_recent <- filter(df_plot, temporal == 2013, variable == "va_ind_pc") %>%
-                    arrange(desc(gdp_pc))
-
-df_plot$spatial <- ordered(df_plot$spatial, levels = df_plot_recent$spatial)
-
-ggplot() +
-  geom_point(data = df_plot, aes(x = gdp_pc, y = value, colour = variable)) +
-  # geom_hline(yintercept = 10) +
-  theme_bw(base_size = 9) +
-  theme(legend.position = "bottom") +
-  ylab("") +
-  xlab("") +
-  facet_wrap( ~ spatial, scales = "free", nrow = 4)
-ggsave(filename = "plots/va_sec_pc_over_gdp_pc.png", height = 20, width = 16, units = "cm")
-
-# plot growth rates over GDP per capita ---
-df_plot <- select(wdi, spatial, temporal, va_agr_pc_gr, va_ind_pc_gr, va_ser_pc_gr, gdp_pc, gdp_pc_gr) %>%
-             filter(spatial %in% g20, gdp_pc_gr >= 0)
-
-plot_alpha = 0.5
-
-ggplot() +
-  theme_bw() +
-  ylab("sectoral growth rate") +
-  # agriculture
-  geom_point(data = df_plot, aes(x = gdp_pc, y = va_agr_pc_gr), colour = "green",
-             alpha = plot_alpha) +
-  # geom_smooth(data = df_plot, aes(x = gdp_pc, y = va_agr_pc_gr), method = lm, colour = "green") +
-  # industry
-  geom_point(data = df_plot, aes(x = gdp_pc, y = va_ind_pc_gr), colour = "blue",
-             alpha = plot_alpha) +
-  # geom_smooth(data = df_plot, aes(x = gdp_pc, y = va_ind_pc_gr), method = lm, colour = "blue") +
-  # services
-  geom_point(data = df_plot, aes(x = gdp_pc, y = va_ser_pc_gr), colour = "red",
-             alpha = plot_alpha) +
-  # geom_smooth(data = df_plot, aes(x = gdp_pc, y = va_ser_pc_gr), method = lm, colour = "red")
-ggsave(filename = "plots/va_sec_pc_gr_over_gdp_pc.png", width = 14, height = 14,
-       units = "cm")
-
 # add recession dummy
 wdi$recession <- NA
 wdi[wdi$gdp_pc_gr > 0 & !(is.na(wdi$gdp_pc_gr)), "recession"] <- 0
@@ -117,3 +68,5 @@ model_agrind <- lm(va_agrind_pc_gr ~ gdp_pc + I(gdp_pc^2) + factor(spatial) +
 model_agrind_nofe <- lm(va_agrind_pc_gr ~ gdp_pc + I(gdp_pc^2) +
                   recession, data = filter(wdi, spatial %in% g20))
 
+# generate plots ----
+source("plotting.R")
---
+++ plotting.R
@@ -0,0 +1,48 @@
+# plot: sectoral value added per capita over gdp per capita ----
+df_plot <- filter(wdi, spatial %in% g20) %>% select(spatial, temporal,
+                                                    va_agr_pc, va_ind_pc,
+                                                    va_ser_pc, gdp_pc)
+
+df_plot <- df_plot[complete.cases(df_plot), ]
+
+df_plot <- melt(df_plot, id.vars = c("spatial", "temporal", "gdp_pc"))
+
+# order countries according to their 2013 GDP levels
+df_plot_recent <- filter(df_plot, temporal == 2013, variable == "va_ind_pc") %>%
+                    arrange(desc(gdp_pc))
+
+df_plot$spatial <- ordered(df_plot$spatial, levels = df_plot_recent$spatial)
+
+ggplot() +
+  geom_point(data = df_plot, aes(x = gdp_pc, y = value, colour = variable)) +
+  # geom_hline(yintercept = 10) +
+  theme_bw(base_size = 9) +
+  theme(legend.position = "bottom") +
+  ylab("") +
+  xlab("") +
+  facet_wrap( ~ spatial, scales = "free", nrow = 4)
+ggsave(filename = "plots/va_sec_pc_over_gdp_pc.png", height = 20, width = 16, units = "cm")
+
+# plot growth rates over GDP per capita ---
+df_plot <- select(wdi, spatial, temporal, va_agr_pc_gr, va_ind_pc_gr, va_ser_pc_gr, gdp_pc, gdp_pc_gr) %>%
+             filter(spatial %in% g20, gdp_pc_gr >= 0)
+
+plot_alpha = 0.5
+
+ggplot() +
+  theme_bw() +
+  ylab("sectoral growth rate") +
+  # agriculture
+  geom_point(data = df_plot, aes(x = gdp_pc, y = va_agr_pc_gr), colour = "green",
+             alpha = plot_alpha) +
+  # geom_smooth(data = df_plot, aes(x = gdp_pc, y = va_agr_pc_gr), method = lm, colour = "green") +
+  # industry
+  geom_point(data = df_plot, aes(x = gdp_pc, y = va_ind_pc_gr), colour = "blue",
+             alpha = plot_alpha) +
+  # geom_smooth(data = df_plot, aes(x = gdp_pc, y = va_ind_pc_gr), method = lm, colour = "blue") +
+  # services
+  geom_point(data = df_plot, aes(x = gdp_pc, y = va_ser_pc_gr), colour = "red",
+             alpha = plot_alpha) +
+  # geom_smooth(data = df_plot, aes(x = gdp_pc, y = va_ser_pc_gr), method = lm, colour = "red")
+ggsave(filename = "plots/va_sec_pc_gr_over_gdp_pc.png", width = 14, height = 14,
+       units = "cm")
