ffb6dabc61fe2a164868f4c18d9abc81bf3548ba
Niklas Roming <niklas.roming@climateanalytics.org>
2017-01-06 11:07:51 +0100

    adjust .gitignore so that it only ignores the copied settings.R-file on the project root directory

---
+++ scripts/settings.R
@@ -0,0 +1,11 @@
+# show how long it took the script to run?
+show_time <- TRUE
+
+# set warning level
+options(warn = -1)
+
+# turn plotting on or off
+plotting <- TRUE
+
+# disable scientific notation
+options(scipen = 999)
