658fc8c86fa0ff30b4cdf24ccfa5802f6e18107b
Niklas Roming <niklas.roming@climateanalytics.org>
2017-01-26 11:54:15 +0100

    include generic vector of econometric model definition and automatic model selection based an adjusted R-squared

--- analysis.R
+++ analysis.R
@@ -29,11 +29,64 @@ if(!dir.exists("output/figures")){
 source("R/prepare_data.R")
 
 # estimation ----
+
+# define right hand side of regression formula
+rhs <- c("gdp_pc + spatial + recession",
+         "gdp_pc + spatial + recession + pop_dens",
+         "gdp_pc + spatial + recession + pop_dens + gdp",
+         "gdp_pc + spatial + recession + pop_dens + pop",
+         "gdp_pc + spatial + recession + pop_dens + gdp + pop",
+         "log(gdp_pc) + spatial + recession",
+         "log(gdp_pc) + spatial + recession + log(gdp)",
+         "log(gdp_pc) + spatial + recession + log(pop)",
+         "gdp_pc + I(gdp_pc^2) + spatial + recession + pop_dens",
+         "gdp_pc + I(gdp_pc^2) + I(gdp_pc^3) + spatial + recession + pop_dens")
+
+# determine formulas
+formula_agr <- paste("va_agr_pc", "~", rhs)
+formula_ind <- paste("va_ind_pc", "~", rhs)
+formula_ser <- paste("va_ser_pc", "~", rhs)
+
+# preallocate lsit for results
+models_agr <- list()
+models_ind <- list()
+models_ser <- list()
+
+# preallocate dataframes for adjusted R-squared
+rsq_agr <- data.frame()
+rsq_ind <- data.frame()
+rsq_ser <- data.frame()
+
+# model selection ----
+for (i in 1:length(rhs)){
+  models_agr[[i]] <- lm(as.formula(formula_agr[i]), data = df)
+  models_ind[[i]] <- lm(as.formula(formula_ind[i]), data = df)
+  models_ser[[i]] <- lm(as.formula(formula_ser[i]), data = df)
+
+  rsq_agr[i, "formula"] <- formula_agr[i]
+  rsq_agr[i, "r_sq_adj"] <- summary(models_agr[[i]])$adj.r.squared
+
+  rsq_ind[i, "formula"] <- formula_ind[i]
+  rsq_ind[i, "r_sq_adj"] <- summary(models_ind[[i]])$adj.r.squared
+
+  rsq_ser[i, "formula"] <- formula_ser[i]
+  rsq_ser[i, "r_sq_adj"] <- summary(models_ser[[i]])$adj.r.squared
+}
+
+# select best model for each sector
+best_agr <- filter(rsq_agr, r_sq_adj == max(r_sq_adj)) %>% select(formula) %>%
+  as.character() %>% as.formula()
+best_ind <- filter(rsq_ind, r_sq_adj == max(r_sq_adj)) %>% select(formula) %>%
+  as.character() %>% as.formula()
+best_ser <- filter(rsq_ser, r_sq_adj == max(r_sq_adj)) %>% select(formula) %>%
+  as.character() %>% as.formula()
+
+# estimation ---
 result_list <- prestimation(x = df,
-                        spatial_ref = country_ref,
-                        formula_agr = va_agr_pc ~ gdp_pc + spatial + recession + pop_dens,
-                     formula_ind = va_ind_pc ~ gdp_pc + I(gdp_pc^2) + I(gdp_pc^3) + spatial + recession + pop_dens,
-                     formula_ser = va_ser_pc ~ gdp_pc + I(gdp_pc^2) + spatial + recession + pop_dens)
+                            spatial_ref = country_ref,
+                            formula_agr = best_agr,
+                            formula_ind = best_ind,
+                            formula_ser = best_ser)
 
 result <- result_list$data
 
