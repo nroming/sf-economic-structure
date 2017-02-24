dfc706208872395f6d4c1f4557c62a0000fab340
Niklas Roming <niklas.roming@climateanalytics.org>
2017-02-24 09:34:42 +0100

    some additions to postprocessing

--- R/post-processing.R
+++ R/post-processing.R
@@ -10,7 +10,20 @@ if(force_sector_match_gdp){
                    # apply these shares to gdp to get adjusted sectoral value added
                    va_agr_pc = va_agr_pc_share * gdp_pc,
                    va_ind_pc = va_ind_pc_share * gdp_pc,
-                   va_ind_pc = va_ind_pc_share * gdp_pc)
+                   va_ind_pc = va_ind_pc_share * gdp_pc,
+
+                   # compute total sector levels
+                   va_sum = va_agr + va_ind + va_ser,
+
+                   va_agr_share = va_agr/va_sum,
+                   va_ind_share = va_ind/va_sum,
+                   va_ser_share = va_ser/va_sum,
+
+                   # apply these shares to gdp to get adjusted sectoral value added
+                   va_agr = va_agr_share * gdp,
+                   va_ind = va_ind_share * gdp,
+                   va_ind = va_ind_share * gdp
+)
 }
 
 
